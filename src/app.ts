20520d96bd086cac3de339b38feb2fd30e23dc96
Paulo Henrique <ph46163835@gmail.com>
2022-10-13 19:47:16 -0300

    feat: config express em app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('iniciando')
+import express from "express";
+require('dotenv').config();
+
+const app = express()
+const PORT = process.env.API_PORT
+
+app.use(express.json())
+
+app.listen(PORT, async () => {
+    console.log(`localhost:${PORT}`)
+})
