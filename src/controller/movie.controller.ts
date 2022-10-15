745d81acc3f08be2872264f84381e63c90873dca
Paulo Henrique <ph46163835@gmail.com>
2022-10-15 17:52:39 -0300

    feat: get movies

--- src/controller/movie.controller.ts
+++ src/controller/movie.controller.ts
@@ -2,13 +2,27 @@ import {Response, Request} from "express"
 import { MovieModel } from "../models/Movie"
 
 import Logger from "../helpers/logger"
+import { nextTick } from "process"
 
 export async function createMovie(request: Request, response: Response) {
     try {
-        const data = request.body
-        const movie = await MovieModel.create(data)
-        return response.status(201).json(movie)
+      const data = request.body
+      const movie = await MovieModel.create(data)
+      return response.status(201).json(movie)
     } catch (e: any) {
-        Logger.error(`Erro no sistema:${e.message}`)
+      Logger.error(`Erro no sistema:${e.message}`)
+    }
+}
+
+export async function getById(request: Request, response: Response) {
+    try {
+      const id = request.params.id
+      const movie = await MovieModel.findById(id)
+      if(!movie) {
+        return response.status(404).json({message: "Não há filme com este id"})
+      }
+      return response.status(200).json(movie)  
+    } catch (e: any) {
+        Logger.error(`Erro no sistema:${e.message}`) 
     }
 }
--- src/router/movie.router.ts
+++ src/router/movie.router.ts
@@ -1,11 +1,12 @@
-import { Router, Request, Response } from "express"
-import { createMovie } from "../controller/movie.controller"
+import { Router } from "express"
+import { createMovie, getById } from "../controller/movie.controller"
 import { validate } from "../middleware/validation"
 import { movieCreateValidation } from "../middleware/validationMovie"
 
 
 const router = Router()
 
+router.get("/movie/:id", getById)
 router.post("/movie", movieCreateValidation(), validate, createMovie)
 
 export default router
