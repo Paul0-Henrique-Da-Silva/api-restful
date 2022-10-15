865c74f520ef284b3ec09f857e0ba3ca154933c0
Paulo Henrique <ph46163835@gmail.com>
2022-10-15 16:12:20 -0300

    feat: add middleware em router /movie

--- src/router/movie.router.ts
+++ src/router/movie.router.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express"
 import { createMovie } from "../controller/movie.controller"
+import { validate } from "../middleware/validation"
+import { movieCreateValidation } from "../middleware/validationMovie"
+
 
 const router = Router()
 
-
-router.post("/movie", createMovie)
-
-
+router.post("/movie", movieCreateValidation(), validate, createMovie)
 
 export default router
